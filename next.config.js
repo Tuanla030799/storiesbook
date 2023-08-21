968823a35c16ba83eaf2fcf502ed953512945e27
Tuanla030799 <tuanla0307@gmail.com>
2023-08-21 23:51:20 +0700

    feat: add i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const { i18n } = require('./next-i18next.config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
+  trailingSlash: true,
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'standalone',
 }
 
 module.exports = nextConfig
---
+++ pages/hello/index.tsx
@@ -0,0 +1,29 @@
+import { i18n, useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { GetServerSideProps } from 'next/types'
+import React from 'react'
+
+type Props = {}
+
+const Hello = (props: Props) => {
+  const { t } = useTranslation(['common'])
+
+  return <div>{t(`hello`)}</div>
+}
+
+export default Hello
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  locale,
+}) => {
+  if (process.env.NODE_ENV === 'development') {
+    await i18n?.reloadResources()
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? '', ['common'])),
+      // Will be passed to the page component as props
+    },
+  }
+}
